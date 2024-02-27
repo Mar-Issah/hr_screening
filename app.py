6b9f1faa78a1b5482980eb4cc684bf25d878a867
Mar-Issah <masy370@gmail.com>
2024-02-27 01:56:32 -0600

    Fix: add a delay of 20secs to fix high latency

--- app.py
+++ app.py
@@ -7,19 +7,18 @@ import uuid
 load_dotenv()
 
 # Creating session variables
-if 'unique_id' not in st.session_state:
-    st.session_state['unique_id'] = ''
+if "unique_id'" not in st.session_state:
+    st.session_state["unique_id"] = ""
 
 def main():
     st.set_page_config(page_title="Resume Screening Assistance", page_icon="📝")
-    st.title("HR - Resume Screening Assistance 💁")
-    st.subheader("I can help you in the resume screening process")
+    st.title("Resume Screening Assistance 💁")
 
     # Text area for job description
-    job_description = st.text_area("Please paste the 'JOB DESCRIPTION' here", key="1")
+    job_description = st.text_area("Enter the 'JOB DESCRIPTION' here", key="1")
 
     # Text input for number of resumes to return
-    document_count = st.text_input("No. of 'RESUMES' to return", key="2", placeholder="2")
+    document_count = st.text_input("Number of 'RESUMES' to return", key="2", placeholder="2")
 
     # Upload resumes
     pdf = st.file_uploader("Upload resumes here, only PDF files allowed", type=["pdf"], accept_multiple_files=True)
@@ -32,14 +31,13 @@ def main():
         st.warning("Please fill in all the inputs to enable analysis.")
 
     if submit:
-        with st.spinner('Wait for it...'):
+        with st.spinner("Wait for it..."):
             try:
-                # Create a unique ID for this session to filtrer out the uploaded
-                st.session_state['unique_id'] = str(uuid.uuid4().hex)
+                # Create a unique ID for this session to filter out docs
+                st.session_state["unique_id"] = str(uuid.uuid4().hex)
 
                 # Create a list of documents from uploaded PDF files
-                docs = create_docs(pdf, st.session_state['unique_id'])
-                st.write(st.session_state['unique_id'])
+                docs = create_docs(pdf, st.session_state["unique_id"])
                 st.write("*Resumes uploaded* :" + str(len(docs)))
 
                 # Create embeddings instance
@@ -47,29 +45,23 @@ def main():
 
                 # Push data to Pinecone
                 docsearch = push_to_pinecone(docs, embeddings)
-                relevant_docs = mmr_search(docsearch, job_description, st.session_state['unique_id'])
+                relevant_docs = similarity_search(docsearch, job_description,document_count, st.session_state["unique_id"])
 
-                # Fetch relevant documents from Pinecone
-                # relevant_docs = similar_docs(job_description, document_count, embeddings, st.session_state['unique_id'])
-                # relevant_docs = similarity(job_description, docsearch)
-                # relevant_docs = similarity(job_description,docsearch,document_count, st.session_state['unique_id'])
-                # st.write(docsearch)
-                st.write(relevant_docs)
-
-				# line seperator
+                # st.write(relevant_docs)
                 st.write(":heavy_minus_sign:" * 30)
 
                 # Display relevant documents
-                # for index, doc_info in enumerate(relevant_docs, start=1):
-                #     st.subheader(f"👉 Document {index}")
-                #     st.write("**File** : " + doc_info[0].metadata['name'])
+                for index, doc_info in enumerate(relevant_docs, start=1):
+                    st.subheader(f"👉 Resume {index}")
+                    st.write("**File** : " + doc_info[0].metadata['name'])
 
-                    # Expander to show more details
-                    # with st.expander('Show me 👀'):
-                    #     st.info("**Match Score** : " + str(doc_info[1]))
-                    #     summary = get_summary(doc_info[0])  # Get summary using LLM
-                    #     st.write("**Summary** : " + summary)
-                st.success("I hope you found the right candidate.❤️")
+					#Expander to show more details
+                    with st.expander('Click to open 👀'):
+                        st.info("**Match Score** : " + str(doc_info[1]))
+                        # Get summary using LLM
+                        summary = get_summary(doc_info[0])
+                        st.write("**Summary** : " + summary)
+                st.success("I hope you find the right candidate.❤️")
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
 
--- utils.py
+++ utils.py
@@ -1,17 +1,12 @@
-from langchain.vectorstores import Pinecone
-# from langchain.llms import OpenAI
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_core.documents import Document
-# import pinecone
 from pypdf import PdfReader
-# # from langchain.llms.openai import OpenAI
-# from langchain.chains.summarize import load_summarize_chain
+from langchain.llms.openai import OpenAI
+from langchain.chains.summarize import load_summarize_chain
 # from langchain.llms import HuggingFaceHub
 from langchain_pinecone import PineconeVectorStore
 import os
-import streamlit as st
-# from pinecone import Pinecone
-# import time
+import time
 
 pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 pinecone_index_name=os.environ.get("PINECONE_INDEX_NAME")
@@ -25,9 +20,7 @@ def get_pdf_text(pdf_doc):
         text += page.extract_text()
     return text
 
-
-# iterate over files in
-# that user uploaded PDF files, one by one
+# iterate over files uploaded
 def create_docs(user_pdf_list, unique_id):
     docs=[]
     for filename in user_pdf_list:
@@ -55,60 +48,17 @@ def push_to_pinecone(docs, embeddings):
 
 
 #Function to help us get relavant documents from vector store - based on user input
-def mmr_search(docsearch,query,unique_id):
-     # similarity_search_with_score returns with score % assign to each seacrh doc
-    k = 1
-    matched_docs = docsearch.similarity_search_with_score(query, int(k), {"unique_id":unique_id.strip()})
-    print(unique_id)
+def similarity_search(docsearch,query,k,unique_id):
+     # similarity_search_with_score returns score % assign to each seacrh doc
+    time.sleep(20)
+    matched_docs = docsearch.similarity_search_with_score(query, int(k), {"unique_id":unique_id})
     return matched_docs
 
 
-#Function to pull infrmation from Vector Store - Pinecone here
-def pull_from_pinecone(pinecone_index_name,embeddings):
-   # Pinecone has eliminated .init method
-    Pinecone(api_key=pinecone_api_key)
-    index = Pinecone.from_existing_index(pinecone_index_name, embeddings)
-    return index
-
-
-#Function to help us get relavant documents from vector store - based on user input
-def similar_docs_pinecone(query, k, embeddings, unique_id):
-    # Pinecone has eliminated .init method
-    Pinecone(api_key=pinecone_api_key)
-    index = pull_from_pinecone(pinecone_index_name,embeddings)
-    # similarity_search_with_score returns with score % assign to each seacrh doc
-    similar_docs = index.similarity_search_with_score(query, int(k), {"unique_id":unique_id})
-    # similar_docs = index.similarity_search(query, filter = {"unique_id":unique_id})
-    return similar_docs
-
-
-
-
-
-
-#-------------------------------------------------------------------------------
-
-#Function to pull infrmation from Vector Store - Pinecone here
-# def pull_from_pinecone(pinecone_index_name,embeddings):
-#     index_name = pinecone_index_name
-#     index = PineconeVectorStore.from_existing_index(index_name, embeddings)
-#     return index
-
-
-
-# Function to help us get relavant documents from vector store - based on user input
-def similar_docs(query,k,embeddings,unique_id):
-    index = pinecone_index_name
-    index = pull_from_pinecone(index,embeddings)
-    similar_docs = index.similarity_search_with_score(query, int(k),{"unique_id":unique_id})
-    #print(similar_docs)
-    return similar_docs
-
-
-# # Helps us get the summary of a document
-# def get_summary(current_doc):
-#     llm = OpenAI(temperature=0)
-#     #llm = HuggingFaceHub(repo_id="bigscience/bloom", model_kwargs={"temperature":1e-10})
-#     chain = load_summarize_chain(llm, chain_type="map_reduce")
-#     summary = chain.run([current_doc])
-#     return summary
+# Summarise doc
+def get_summary(current_doc):
+    llm = OpenAI(temperature=0)
+    #llm = HuggingFaceHub(repo_id="bigscience/bloom", model_kwargs={"temperature":1e-10})
+    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    summary = chain.run([current_doc])
+    return summary
